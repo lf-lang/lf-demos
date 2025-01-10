d67be3d96980e8da539ebb273d04c90b3a93826f
erlingrj <erling.jellum@gmail.com>
2025-01-10 14:56:26 +0100

    More macOS fixes

--- network-monitor/src/lib/HttpServer.cmake
+++ network-monitor/src/lib/HttpServer.cmake
@@ -1,1 +1,6 @@
 target_link_libraries(${LF_MAIN_TARGET} PRIVATE microhttpd)
+
+# The two following lines are needed to find the microhttpd library
+# on macOS. On Linux, the library is found automatically.
+target_include_directories(${LF_MAIN_TARGET} PRIVATE "/usr/local/include")
+target_link_directories(${LF_MAIN_TARGET} PRIVATE "/usr/local/lib")
