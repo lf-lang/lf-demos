a8ce2dafc63c2379e109c99a997ae03d7bf4b4d3
erlingrj <erling.jellum@gmail.com>
2025-01-13 13:09:12 +0100

    Try to fix HttpServer.cmake for macos

--- network-monitor/src/lib/HttpServer.cmake
+++ network-monitor/src/lib/HttpServer.cmake
@@ -1,6 +1,9 @@
-target_link_libraries(${LF_MAIN_TARGET} PRIVATE microhttpd)
 
 # The two following lines are needed to find the microhttpd library
 # on macOS. On Linux, the library is found automatically.
-target_include_directories(${LF_MAIN_TARGET} PRIVATE "/usr/local/include")
-target_link_directories(${LF_MAIN_TARGET} PRIVATE "/usr/local/lib")
+list(APPEND CMAKE_PREFIX_PATH /usr/local /opt/brew)
+find_library(MICROHTTPD_LIB microhttpd)
+find_path(MICROHTTPD_INCLUDE_DIR microhttpd)
+
+target_link_libraries(${LF_MAIN_TARGET} PRIVATE microhttpd)
+target_include_directories(${LF_MAIN_TARGET} PRIVATE {MICROHTTPD_INCLUDE_DIR})
